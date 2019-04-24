6284b55fc736fcc36cbb4f841bf28ba72da33cd4
Kevin Reaves <kevin82593@hotmail.com>
2019-04-24 16:03:47 -0500

    got print working for filterArray

--- haskell/filterArray.hs
+++ haskell/filterArray.hs
@@ -3,9 +3,11 @@
 f:: Int -> [Int] -> [Int]
 f n arr = filter (\x -> x < n) arr
 
+# doesn't like to be printed with ghci, need to compile with ghc
 main = do
     n <- readLn :: IO Int
     inputdata <- getContents
     let 
         numbers = map read (lines inputdata) :: [Int]
-    putStrLn . unlines $ (map show . f n ) numbers
+    let newNumbers =  (map show . f n ) numbers
+    print(newNumbers)
