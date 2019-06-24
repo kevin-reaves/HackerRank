55d7018c7fe74cd35832f6dcc4800da6a19f0602
Kevin Reaves <kevin82593@hotmail.com>
2019-06-23 20:46:55 -0500

    hacker rank c#

---
+++ c#/HackerRank/Arrays/Hourglass.cs
@@ -0,0 +1,14 @@
+﻿// https://www.hackerrank.com/challenges/2d-array/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+
+using System;
+
+namespace Arrays
+{
+    class Hourglass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
