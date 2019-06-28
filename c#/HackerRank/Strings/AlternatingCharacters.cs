00e14da32d287b5ab4a5f2164ad9a43c3a6c984d
Kevin Reaves <kevin82593@hotmail.com>
2019-06-27 23:46:47 -0500

    alternating characters c#

---
+++ c#/HackerRank/Strings/AlternatingCharacters.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Strings
+{
+    class Program
+    {
+        // https://www.hackerrank.com/challenges/alternating-characters/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=strings
+        static int CountRemovedCharacters(string dupString)
+        {
+            int result = 0;
+            int look = dupString.Length;
+            // Can't check element n + 1, so we only need to go to -1 length
+            for (int i = 0; i < dupString.Length -1; i++)
+            {
+                if (dupString[i] == dupString[i + 1])
+                {
+                    result += 1;
+                }
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            string duplicateString = "AAABBB";
+            int result = CountRemovedCharacters(duplicateString);
+            Console.WriteLine(result);
+        }
+    }
+}
